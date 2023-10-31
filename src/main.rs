d4ce76e3e1040f245a76af3b3d40d758a17e3a7c
Can A <26111053+AmonKoth@users.noreply.github.com>
2023-10-31 15:10:10 +0100

    Separated ui and general rendering
    Separated the ui rendering from general rendering cause UI rendering method takes too much processing power if done in every frame

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
-use sdl2::render::{TextureCreator, WindowCanvas};
-use sdl2::video::WindowContext;
+use sdl2::render::{Texture, WindowCanvas};
 use specs::{DispatcherBuilder, Join, World, WorldExt};
 
 use std::collections::HashMap;
@@ -28,9 +27,8 @@ const SCREEN_HEIGHT: i32 = 600;
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
-    texture_creator: &TextureCreator<WindowContext>,
     texture_manager: &texture_manager::TextureManager,
-    font: &sdl2::ttf::Font,
+    ui_elements: &Vec<UIElement>,
     ecs: &World,
 ) -> Result<(), String> {
     canvas.set_draw_color(color);
@@ -38,27 +36,13 @@ fn render(
 
     let positions = ecs.read_storage::<components::Position>();
     let mut renderables = ecs.write_storage::<components::Renderable>();
-    let players = ecs.read_storage::<components::Player>();
 
-    {
-        let render_count_text = "Entity amount: ".to_string() + &renderables.count().to_string();
-        let text_position = Rect::new(SCREEN_WIDTH - 200, 0, 200, 40);
-        render_text(
-            &render_count_text,
-            text_position,
-            Color::RGBA(255, 0, 0, 255),
-            texture_creator,
-            font,
-            canvas,
-        )?;
-    }
     for (renderable, position) in (&mut renderables, &positions).join() {
         let screen_rect = Rect::from_center(
             position.pos,
             renderable.output_width,
             renderable.output_height,
         );
-        // let texture = texture_creator.load_texture(&renderable.texture_name)?;
         let texture = texture_manager.get_texture(&renderable.texture_name)?;
         let src = Rect::new(
             (renderable.input_width * renderable.frame) as i32,
@@ -78,79 +62,11 @@ fn render(
         renderable.frame = (renderable.frame + 1) % renderable.total_frames;
     }
 
-    let gamedatas = ecs.read_storage::<components::GameData>();
-    for gamedata in (gamedatas).join() {
-        {
-            let score: String = "Score: ".to_string() + &gamedata.score.to_string();
-            let score_text_pos = Rect::new(10 as i32, 0 as i32, 100 as u32, 50 as u32);
-            render_text(
-                &score,
-                score_text_pos,
-                Color::RGBA(255, 0, 0, 255),
-                texture_creator,
-                font,
-                canvas,
-            )?;
-
-            let level: String = "Level: ".to_string() + &gamedata.level.to_string();
-            let level_text_pos =
-                Rect::new(SCREEN_WIDTH / 2 as i32, 0 as i32, 100 as u32, 50 as u32);
-            render_text(
-                &level,
-                level_text_pos,
-                Color::RGBA(255, 0, 0, 255),
-                texture_creator,
-                font,
-                canvas,
-            )?;
-        }
-    }
-    for player in (players).join() {
-        let immortality_text: String =
-            "Press C to toggle godmode : ".to_string() + &(!player.can_take_damage).to_string();
-        let text_pos = Rect::new(10, SCREEN_HEIGHT - 50, 300, 50);
-        render_text(
-            &immortality_text,
-            text_pos,
-            Color::RGBA(0, 255, 0, 255),
-            texture_creator,
-            font,
-            canvas,
-        )?;
-        {
-            let spawn_text: String = "Press V to spawn 20 enemies".to_string();
-            let text_pos = Rect::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50, 300, 50);
-            render_text(
-                &spawn_text,
-                text_pos,
-                Color::RGBA(0, 255, 0, 255),
-                texture_creator,
-                font,
-                canvas,
-            )?;
-        }
+    for ui_element in ui_elements {
+        canvas.copy(&ui_element.texture, None, Some(ui_element.position))?;
     }
 
     canvas.present();
-    Ok(())
-}
-
-fn render_text(
-    text: &String,
-    text_position: Rect,
-    color: Color,
-    texture_creator: &TextureCreator<WindowContext>,
-    font: &sdl2::ttf::Font,
-    canvas: &mut WindowCanvas,
-) -> Result<(), String> {
-    let surface = font
-        .render(text)
-        .blended(color)
-        .map_err(|e| e.to_string())?;
-    let texture = texture_creator
-        .create_texture_from_surface(&surface)
-        .map_err(|e| e.to_string())?;
-    canvas.copy(&texture, None, Some(text_position))?;
     Ok(())
 }
 
@@ -193,26 +109,16 @@ struct State {
     ecs: World,
 }
 
+struct UIElement<'a> {
+    texture: Texture<'a>,
+    position: Rect,
+}
+
 fn main() -> Result<(), String> {
     println!("Starting Astroids Game");
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
-    // let image_context = sdl2::image::init(InitFlag::JPG | InitFlag::PNG)?;
-
-    // let src = Rect::new(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
-
-    // let x: i32 = (SCREEN_WIDTH) / 2 as i32;
-    // let y: i32 = (SCREEN_HEIGHT) / 2 as i32;
-
-    // let dst = Rect::new(
-    //     x - ((OUTPUTH_WIDTH / 2) as i32),
-    //     y - ((OUTPUTH_HEIGHT / 2) as i32),
-    //     OUTPUTH_WIDTH,
-    //     OUTPUTH_HEIGHT,
-    //  );
-    // let center = Point::new((SCREEN_WIDTH / 2) as i32, (SCREEN_HEIGHT / 2) as i32);
 
     let window = video_subsystem
         .window("Astroids", SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32)
@@ -264,6 +170,10 @@ fn main() -> Result<(), String> {
     let mut frame_count = 0;
     let mut last_second = Instant::now();
 
+    let max_ui_render_wait = 100;
+    let mut ui_render_wait = max_ui_render_wait;
+    let mut ui_storage: Vec<UIElement> = Vec::new();
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -302,27 +212,120 @@ fn main() -> Result<(), String> {
         game_state.ecs.maintain();
         frame_count += 1;
         let elapsed_time = last_second.elapsed().as_secs_f64();
-        let fps_text_pos = Rect::new(SCREEN_WIDTH - 200, 200, 20, 20);
+
         if elapsed_time >= 1.0 {
             let fps = frame_count as f64 / elapsed_time;
             println!("FPS :{:.2}", fps);
-            render_text(
-                &fps.to_string(),
-                fps_text_pos,
-                Color::RGBA(255, 0, 0, 255),
-                &texture_creator,
-                &font,
-                &mut canvas,
-            )?;
             frame_count = 0;
             last_second = Instant::now();
+        }
+        ui_render_wait += 1;
+        if ui_render_wait >= max_ui_render_wait {
+            ui_render_wait = 0;
+            ui_storage.clear();
+            {
+                let players = game_state.ecs.read_storage::<crate::components::Player>();
+                for player in (players).join() {
+                    let immortality_text: String = "Press C to toggle godmode : ".to_string()
+                        + &(!player.can_take_damage).to_string();
+                    let text_pos = Rect::new(10, SCREEN_HEIGHT - 50, 300, 50);
+                    let surface = font
+                        .render(&immortality_text)
+                        .solid(Color::RGBA(0, 255, 0, 255))
+                        .map_err(|e| e.to_string())?;
+                    let texture = texture_creator
+                        .create_texture_from_surface(&surface)
+                        .map_err(|e| e.to_string())?;
+                    let ui_element = UIElement {
+                        texture: texture,
+                        position: text_pos,
+                    };
+
+                    ui_storage.push(ui_element);
+                }
+            }
+            {
+                let gamedatas = game_state.ecs.read_storage::<components::GameData>();
+                for gamedata in (gamedatas).join() {
+                    {
+                        {
+                            let score: String = "Score: ".to_string() + &gamedata.score.to_string();
+                            let score_text_pos =
+                                Rect::new(10 as i32, 0 as i32, 100 as u32, 50 as u32);
+                            let surface = font
+                                .render(&score)
+                                .solid(Color::RGBA(255, 0, 0, 255))
+                                .map_err(|e| e.to_string())?;
+                            let texture = texture_creator
+                                .create_texture_from_surface(&surface)
+                                .map_err(|e| e.to_string())?;
+                            let ui_element = UIElement {
+                                texture: texture,
+                                position: score_text_pos,
+                            };
+
+                            ui_storage.push(ui_element);
+                        }
+                        {
+                            let level: String = "Level: ".to_string() + &gamedata.level.to_string();
+                            let level_text_pos =
+                                Rect::new(SCREEN_WIDTH / 2 as i32, 0 as i32, 100 as u32, 50 as u32);
+                            let surface = font
+                                .render(&level)
+                                .solid(Color::RGBA(255, 0, 0, 255))
+                                .map_err(|e| e.to_string())?;
+                            let texture = texture_creator
+                                .create_texture_from_surface(&surface)
+                                .map_err(|e| e.to_string())?;
+                            let ui_element = UIElement {
+                                texture: texture,
+                                position: level_text_pos,
+                            };
+                            ui_storage.push(ui_element);
+                        }
+                    }
+                }
+            }
+
+            {
+                let render_count_text = "Entity amount: ".to_string()
+                    + &game_state.ecs.entities().join().count().to_string();
+                let text_pos = Rect::new(SCREEN_WIDTH - 200, 0, 200, 40);
+                let surface = font
+                    .render(&render_count_text)
+                    .solid(Color::RGBA(255, 0, 0, 255))
+                    .map_err(|e| e.to_string())?;
+                let texture = texture_creator
+                    .create_texture_from_surface(&surface)
+                    .map_err(|e| e.to_string())?;
+                let ui_element = UIElement {
+                    texture: texture,
+                    position: text_pos,
+                };
+                ui_storage.push(ui_element);
+            }
+            {
+                let spawn_text: String = "Press V to spawn 20 enemies".to_string();
+                let text_pos = Rect::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50, 300, 50);
+                let surface = font
+                    .render(&spawn_text)
+                    .solid(Color::RGBA(0, 255, 0, 255))
+                    .map_err(|e| e.to_string())?;
+                let texture = texture_creator
+                    .create_texture_from_surface(&surface)
+                    .map_err(|e| e.to_string())?;
+                let ui_element = UIElement {
+                    texture: texture,
+                    position: text_pos,
+                };
+                ui_storage.push(ui_element);
+            }
         }
         render(
             &mut canvas,
             Color::RGB(0, 0, 0),
-            &texture_creator,
             &texture_manager,
-            &font,
+            &ui_storage,
             &game_state.ecs,
         )?;
 
